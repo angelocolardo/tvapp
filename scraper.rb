8fc1e84178cc478d3df43c2a1688338c007c6ed6
angelo colardo <angelocolardo@gmail.com>
2019-06-23 16:43:34 +0200

    got right hash

--- scraper.rb
+++ scraper.rb
@@ -2,36 +2,35 @@ require 'open-uri'
 require 'nokogiri'
 require 'yaml'
 
-ingredient = 'chocolate'
-url = "https://www.staseraintv.com/index.html"
-
-
+# ingredient = 'chocolate'
+url = 'https://www.staseraintv.com/index.html'
+# def scrapeit(url)
 html_file = open(url).read
 html_doc = Nokogiri::HTML(html_file)
 
-
-  html_doc.search('.listingprevbox').each do |element|
-   # puts element.text.strip
-    ch_title = []
-    ch_title << element.text.strip.split("\n").first
-    p ch_title.first
-  end
+# TITLE SCRAPER
+html_doc.search('.listingprevbox').each do |element|
+  @ch_title = []
+  @ch_title << element.text.strip.split("\n").first
 
 
-# html_doc.search('.listingprevbox p').each do |element|
-#   puts element.text.strip
-#   @ch_prog = element.text.strip
-#   # puts element.text.strip
-#   # puts element.attribute('href').value
+# CONTENT SCRAPER
+html_doc.search('.listingprevbox p').each do |element|
+  @ch_progs = element.text.strip.split("\n")
+
+  progs = {
+    title: @ch_title,
+    programs: @ch_progs
+  }
+  p progs
+end
+end
 # end
 
-  # {
-  #   title: @ch_title,
-  #   programs: @ch_prog
-  # }
+# scrapeit(url)
 
 # puts "writing into db"
 # File.open("progs.yml", "w") do |f|
-#   f.write(@progs.to_yaml)
+#   f.write(progs.to_yaml)
 # end
 # puts "done, dude!"
